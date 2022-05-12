d72fcbac03a2f7b68667a50cc1f5158475f5e0fe
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-12 20:33:23 +0530

    Added endpoints

--- main.py
+++ main.py
@@ -20,5 +20,27 @@ soup=BeautifulSoup(htmlContent,'html.parser')
 
 # Step 3 : HTML Tree traversal
 title=soup.title
-print(title)
-print(type(title))
+# print(title)
+# print(type(title))
+
+
+# Get all the paragraphs from the page
+paras=soup.find_all('p')
+#print(paras)
+
+
+# Get all the anchor tags from the page
+anchors=soup.find_all('a')
+#print(anchors)
+
+
+# find first paragraph
+para=soup.find('p')
+#print(para)
+
+# find class of first para
+#print(soup.find('p')['class'])
+
+
+# find all the elements with class lead
+print(soup.find_all("p",class_="lead"))
