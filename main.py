4cb13395610164ca58b885aa4cd5fc5c5b596840
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-12 21:50:17 +0530

    web scrapping Part-1 completed

--- main.py
+++ main.py
@@ -44,15 +44,50 @@ para=soup.find('p')
 
 # Get the text from the tags/soup
 #print(soup.find('p').get_text())
-print(soup.get_text())
+#print(soup.get_text())
 
 
 # find all the elements with class lead
-print(soup.find_all("p",class_="lead"))
+#print(soup.find_all("p",class_="lead"))
 
 
 # get all the links in the page
 
+all_links=set()
 anchors=soup.find_all('a')
 for link in anchors:
-    print(link.get('href'))
+    if(link.get('href')!='#'):
+        link_text="https://codewithharry.com"+link.get('href')
+        all_links.add("https://codewithharry.com"+link.get('href'))
+        #print(link.get('href'))
+        #print(link_text)
+
+#print(all_links)
+
+
+# getting an element with specific id
+
+navbarSupportContent=soup.find(id="lo-engage-ext-container")
+#print(navbarSupportContent)
+
+# for elem in navbarSupportContent.contents:
+#     print(elem)
+
+
+# for item in navbarSupportContent.strings:
+#     print(item)
+
+
+# for item in navbarSupportContent.stripped_strings:
+#     print(item)
+
+# for item in navbarSupportContent.parents:
+#     print(item)
+
+
+# used to directly get the code of the id or class
+elem=soup.select('#loginModal')
+print(elem)
+
+elem=soup.select('.modal-footer')
+print(elem)
